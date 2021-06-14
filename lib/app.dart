011bc48b9e62a92cca95f61ea97a95f8b3e00d1b
Filipe Morelli <morellitecinfo@gmail.com>
2021-06-14 14:42:58 -0300

    implementing providers

--- lib/app.dart
+++ lib/app.dart
@@ -1,22 +1,29 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx_template/flavors.dart';
 import 'package:flutter_mobx_template/routes/route_names.dart';
 import 'package:flutter_mobx_template/routes/routes.dart';
+import 'package:flutter_mobx_template/services/api.dart';
+import 'package:flutter_mobx_template/services/providers.dart';
 import 'package:flutter_mobx_template/themes/dark_theme.dart';
 import 'package:flutter_mobx_template/themes/light_theme.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: F.title,
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      initialRoute: RouteNames.initialRoute,
-      routes: routes,
+    return MultiProvider(
+      providers: providers,
+      child: MaterialApp(
+        title: F.title,
+        debugShowCheckedModeBanner: false,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        initialRoute: RouteNames.initialRoute,
+        routes: routes,
+      ),
     );
   }
 }
