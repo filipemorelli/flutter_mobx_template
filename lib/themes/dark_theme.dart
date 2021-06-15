9767e67319f3922bef3e18d30d9ee1c21f90cadb
Filipe Morelli <morellitecinfo@gmail.com>
2021-06-15 11:51:32 -0300

    Snackbar themes

--- lib/themes/dark_theme.dart
+++ lib/themes/dark_theme.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
-final ThemeData darkTheme = ThemeData.dark();
+final ThemeData darkTheme = ThemeData.dark().copyWith(
+  snackBarTheme: const SnackBarThemeData(
+    behavior: SnackBarBehavior.floating,
+  ),
+);
--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = ThemeData.light().copyWith(
   primaryColor: Colors.indigo,
+  snackBarTheme: const SnackBarThemeData(
+    behavior: SnackBarBehavior.floating,
+  ),
 );
