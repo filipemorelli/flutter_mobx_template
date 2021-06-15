2530d640f14dc932ddc26238fd31bb392ac763ea
Filipe Morelli <morellitecinfo@gmail.com>
2021-06-15 11:56:41 -0300

    fix routes providers

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx_template/modules/posts/view_model/post_view_model.dart';
+import 'package:flutter_mobx_template/modules/posts/views/post_view.dart';
+import 'package:flutter_mobx_template/repository/implementation/post_repository.dart';
 import 'package:flutter_mobx_template/routes/route_names.dart';
-import 'package:flutter_mobx_template/views/post_view.dart';
+import 'package:provider/provider.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  RouteNames.posts: (BuildContext context) => const PostView(),
+  RouteNames.posts: (BuildContext context) => PostView(
+        postViewModel: PostViewModel(
+          Provider.of<PostRepository>(context, listen: false),
+        ),
+      ),
 };
--- lib/services/providers.dart
+++ lib/services/providers.dart
@@ -1,10 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx_template/flavors.dart';
+import 'package:flutter_mobx_template/repository/implementation/post_repository.dart';
 import 'package:flutter_mobx_template/services/api.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 final List<SingleChildWidget> providers = <Provider<dynamic>>[
-  Provider<Dio>(create: (BuildContext ctx) => Api(F.serverHost).init()),
+  Provider<Dio>(
+    lazy: false,
+    create: (BuildContext ctx) => Api(F.serverHost).init(),
+  ),
+  Provider<PostRepository>(
+    create: (BuildContext ctx) => PostRepository(ctx.read<Dio>()),
+  ),
 ];
