f94fd308eddaf55b448df35aec01fbe751a51e33
Filipe Morelli <morellitecinfo@gmail.com>
2021-06-21 15:26:21 -0300

    set git secret

--- lib/env/env_prod.dart
+++
@@ -1,4 +0,0 @@
-const Map<String, dynamic> envProd = <String, dynamic>{
-  'title': 'SecretWall',
-  'baseUrl': 'http://192.168.2.110:3000'
-};
--- lib/env/env_qa.dart
+++
@@ -1,4 +0,0 @@
-const Map<String, dynamic> envQa = <String, dynamic>{
-  'title': 'SecretWall QA',
-  'baseUrl': 'http://192.168.2.110:3000'
-};
